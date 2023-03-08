75df7d0e8a8c97e8385a1b03e94312bfbfde8957
aa235553756 <aa235553756@gmail.com>
2023-03-08 08:35:29 +0800

    新增地區,類別篩選器,及json瀏覽

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,173 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import { journeyList, spots } from './新文字文件'
+import { useForm } from "react-hook-form";
+// import { journeyList, spots } from './新文字文件'
+import { data } from '../src/士林'
 
 function App() {
   const [num, setNum] = useState(1);
-  const [blur, setBlue] = useState('blur(2px)')
-  console.log(journeyList[num]['journey'][0]['name'], spots)
+  const [blur, setBlue] = useState('none')
+  const [renderData, setRenderData] = useState([])
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = formData => {
+    console.log(formData);
+    setRenderData(
+      getData(formData)
+    )
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function getData(formData) {
+    let renderData = []
+    for (let i = 0; renderData.length < 4; i++) {
+      let num = getRandomInt(data.length)
+
+      let firstCheck = false;
+      let secondCheck = false;
+
+      formData.Distric.map((item) => {
+        if (item === '0') {
+          firstCheck = true
+        }
+        if (data[num]["Distric"] === item) {
+          firstCheck = true
+        }
+      })
+
+      formData.Type.map((item) => {
+        if (item === '0') {
+          secondCheck = true
+        }
+        if (data[num]["Type"].includes(Number(item))) {
+          secondCheck = true
+        }
+      })
+
+      if (firstCheck && secondCheck) {
+        if (!renderData.includes(data[num])) {
+          renderData.push(data[num])
+        }
+      }
+    } return renderData
+  }
 
   return (
     <>
-      <div className="App" style={{ outline: '0px solid black' }} >
-        <div className="navbar" style={{ height: '120px', margin: '0 auto' }}>
-          <div className="container" style={{ maxWidth: '1140px', display: 'flex', justifyContent: 'space-between', margin: '0 auto' }}>
-            <div>
-              <img src="" alt="logo" className="logo" />
-              <span>名稱名稱</span>
-            </div>
-            <ul style={{ display: 'flex', gap: '32px' }}>
-              <li>我的搜尋</li>
-              <li>規劃行程</li>
-              <li>我的收藏</li>
-              <li>登入/註冊</li>
-            </ul>
+      <div className="App" style={{ display: 'flex', padding: '16px', outline: '0px solid black', maxHeight: '100vh' }} >
+        <form style={{ margin: "auto 0", display: 'flex', alignItems: 'center', gap: '16px' }} onSubmit={handleSubmit(onSubmit)}>
+          <div id="checkbox" style={{ display: 'flex', flexDirection: 'column' }}>
+            <label><input type="checkbox" value="0" {...register("Type")} /><span className="round button">隨心所欲</span></label>
+            <label><input type="checkbox" value="1" {...register("Type")} /><span className="round button">城市走走1</span></label>
+            <label><input type="checkbox" value="2" {...register("Type")} /><span className="round button">拍照聖地2</span></label>
+            <label><input type="checkbox" value="3" {...register("Type")} /><span className="round button">放鬆療癒3</span></label>
+            <label><input type="checkbox" value="4" {...register("Type")} /><span className="round button">夜間首選4</span></label>
+            <label><input type="checkbox" value="5" {...register("Type")} /><span className="round button">文藝青年5</span></label>
+            <label><input type="checkbox" value="6" {...register("Type")} /><span className="round button">親子互動6</span></label>
+            <label><input type="checkbox" value="7" {...register("Type")} /><span className="round button">冒險活潑7</span></label>
           </div>
-        </div>
-        <div className="banner">
-          <h1 className='title' style={{ textAlign: 'left', width: '744px', margin: '0 auto' }}>規劃行程不必擔心,只要動動手指<br />五秒鐘讓你立即準備出發！</h1>
-          <h2 style={{ fontSize: '40px' }}>說走就走,開啟一場隨機出發的旅行</h2>
-          <div className="select" style={{ width: '744px', height: '86px', background: 'gray', margin: '0 auto' }} >
-            <div className='selectBlock'>行程/距離</div>
-            <div className='selectBlock'>請選擇區域</div>
-            <button className='selectBlock' style={{ background: 'lightgray' }}>開始規劃</button>
+          <div id="checkbox" style={{ display: 'flex', flexDirection: 'column' }}>
+            <label><input type="checkbox" value="0"  {...register("Distric")} /><span className="round button">不限</span></label>
+            <label><input type="checkbox" value="士林區" {...register("Distric")} /><span className="round button">士林區</span></label>
+            <label><input type="checkbox" value="中山區" {...register("Distric")} /><span className="round button">中山區</span></label>
+            <label><input type="checkbox" value="信義區" {...register("Distric")} /><span className="round button">信義區</span></label>
+            <label><input type="checkbox" value="北投區" {...register("Distric")} /><span className="round button">北投區</span></label>
+            <label><input type="checkbox" value="大安區" {...register("Distric")} /><span className="round button">大安區</span></label>
+            <label><input type="checkbox" value="中正區" {...register("Distric")} /><span className="round button">中正區</span></label>
+            <label><input type="checkbox" value="士林區" {...register("Distric")} /><span className="round button">士林區</span></label>
+            <label><input type="checkbox" value="大同區" {...register("Distric")} /><span className="round button">大同區</span></label>
+            <label><input type="checkbox" value="內湖區" {...register("Distric")} /><span className="round button">內湖區</span></label>
+            <label><input type="checkbox" value="萬華區" {...register("Distric")} /><span className="round button">萬華區</span></label>
+            <label><input type="checkbox" value="松山區" {...register("Distric")} /><span className="round button">松山區</span></label>
+            <label><input type="checkbox" value="南港區" {...register("Distric")} /><span className="round button">南港區</span></label>
           </div>
-        </div>
-        <div className="generater">
+          <button className='selectBlock' style={{ display: 'flex', gap: '12px', paddingLeft: '16px', background: '#737373', color: 'white' }}>
+            <img src="undo.png" alt="" style={{ width: '24px' }} />
+            換一組
+          </button>
+        </form>
+        <div className="generater" style={{ padding: '0 24px' }}>
           <div className='generaterBlock' style={{ display: 'flex', maxWidth: '1140px', margin: '0 auto' }}>
-            <div style={{ textAlign: 'left' }}>
-              <h2 style={{ marginBottom: '20px' }}>我想去哪裡...</h2>
-              <h3 style={{ fontSize: '24px', marginBottom: '40px' }}>開啟隨機產生器，讓你每天生活有樂趣。</h3>
-              <div style={{ display: 'flex', alignItems: 'end', justifyContent: 'space-between' }}>
-                <button className='selectBlock' style={{ display: 'flex', gap: '12px', paddingLeft: '56px', background: '#737373', color: 'white' }}
-                  onClick={() => {
-                    setNum((prev) => {
-                      if (prev === 2) {
-                        return 0
-                      }
-                      return prev + 1
-                    })
-                    setBlue((prve) => {
-                      return 'blur(5px)'
-                    })
-                    setTimeout(() => {
-                      setBlue((prve) => {
-                        return 'none';
-                      })
-                    }, 2000)
-                  }}
-                >
-                  <img src="undo.png" alt="" style={{ width: '24px' }} />
-                  換一組
-                </button>
-                <h3 style={{ marginRight: '-36px', marginBottom: '4px' }}><i>{journeyList[num]['type']}</i></h3>
-              </div>
-            </div>
-            <ul className='imgList' style={{ display: 'flex', gap: '20px', filter: blur }}>
+            <ul className='imgList' style={{ display: 'flex', flexDirection: 'column', gap: '20px', filter: blur, width: '150px' }}>
               <li>
-                <img src={journeyList[num]['journey'][0]['imgUrl']} alt="" />
-                <h3>{journeyList[num]['journey'][0]['name']}</h3>
+                <img src={renderData?.[0]?.Images?.Src?.[0] ? renderData?.[0]?.Images?.Src?.[0] : "https://picsum.photos/200"} alt="" />
+                <p>{renderData?.[0]?.['AttractionName']}</p>
               </li>
               <li>
-                <img src={journeyList[num]['journey'][1]['imgUrl']} alt="" />
-                <h3>{journeyList[num]['journey'][1]['name']}</h3>
+                <img src={renderData?.[1]?.Images?.Src?.[0] ? renderData?.[1]?.Images?.Src?.[0] : "https://picsum.photos/200"} alt="" />
+                <p>{renderData?.[1]?.['AttractionName']}</p>
               </li>
               <li>
-                <img src={journeyList[num]['journey'][2]['imgUrl']} alt="" />
-                <h3>{journeyList[num]['journey'][2]['name']}</h3>
+                <img src={renderData?.[2]?.Images?.Src?.[0] ? renderData?.[2]?.Images?.Src?.[0] : "https://picsum.photos/200"} alt="" />
+                <p>{renderData?.[2]?.['AttractionName']}</p>
               </li>
               <li>
-                <img src={journeyList[num]['journey'][3]['imgUrl']} alt="" />
-                <h3>{journeyList[num]['journey'][3]['name']}</h3>
+                <img src={renderData?.[3]?.Images?.Src?.[0] ? renderData?.[3]?.Images?.Src?.[0] : "https://picsum.photos/200"} alt="" />
+                <p>{renderData?.[3]?.['AttractionName']}</p>
               </li>
             </ul>
           </div>
         </div>
-      </ div >
+        <ul style={{ display: 'flex', flexDirection: 'column', gap: '24px', maxWidth: '500px', overflow: 'auto' }}>
+          {renderData.map((item) => {
+            return <li style={{ width: '450px', listStyle: 'auto', textAlign: 'left' }}>{JSON.stringify(item)}</li>
+          })}
+        </ul>
+      </div >
     </>
   )
 }
 
 export default App
+
+function Navbar({ }) {
+  return (<><div className="navbar" style={{
+    height: '120px',
+    margin: '0 auto'
+  }}>
+    <div className="container" style={{
+      maxWidth: '1140px',
+      display: 'flex',
+      justifyContent: 'space-between',
+      margin: '0 auto'
+    }}>
+      <div>
+        <img src="" alt="logo" className="logo" />
+        <span>名稱名稱</span>
+      </div>
+      <ul style={{
+        display: 'flex',
+        gap: '32px'
+      }}>
+        <li>我的搜尋</li>
+        <li>規劃行程</li>
+        <li>我的收藏</li>
+        <li>登入/註冊</li>
+      </ul>
+    </div>
+  </div>
+    <div className="banner">
+      <h1 className='title' style={{
+        textAlign: 'left',
+        width: '744px',
+        margin: '0 auto'
+      }}>規劃行程不必擔心,只要動動手指<br />五秒鐘讓你立即準備出發！</h1>
+      <h2 style={{
+        fontSize: '40px'
+      }}>說走就走,開啟一場隨機出發的旅行</h2>
+      <div className="select" style={{
+        width: '744px',
+        height: '86px',
+        background: 'gray',
+        margin: '0 auto'
+      }}>
+        <div className='selectBlock'>行程/距離</div>
+        <div className='selectBlock'>請選擇區域</div>
+        <button className='selectBlock' style={{
+          background: 'lightgray'
+        }}>開始規劃</button>
+      </div>
+    </div></>);
+}
--- src/新文字文件.js
+++ src/新文字文件.js
@@ -1,132 +1,174 @@
-export const journeyList = [{
-  name: '洧杰行程',
-  type: '夜間首選',
-  journey: [{
-    name: '海水屋100元熱炒',
-    imgUrl: 'https://picsum.photos/140',
-  }, {
-    name: '渣男串燒',
-    imgUrl: 'https://picsum.photos/141',
-  }, {
-    name: '錢櫃KTV',
-    imgUrl: 'https://picsum.photos/142',
-  }, {
-    name: '新永和豆漿',
-    imgUrl: 'https://picsum.photos/143',
-  }]
-}, {
-  name: '筱清行程',
-  type: '放鬆養生',
-  journey: [{
-    name: '楓上SPA會館',
-    imgUrl: 'https://picsum.photos/144',
-  }, {
-    name: '碳佐麻里燒肉',
-    imgUrl: 'https://picsum.photos/145',
-  }, {
-    name: '白河溫泉',
-    imgUrl: 'https://picsum.photos/146',
-  }, {
-    name: '山野Bar',
-    imgUrl: 'https://picsum.photos/147',
-  }]
-}, {
-  name: 'Tina行程',
-  type: '城市郊遊',
-  journey: [{
-    name: '早安公雞',
-    imgUrl: 'https://picsum.photos/148',
-  }, {
-    name: '凱擘影院',
-    imgUrl: 'https://picsum.photos/149',
-  }, {
-    name: '後站商圈_府中',
-    imgUrl: 'https://picsum.photos/150',
-  }, {
-    name: '秘境咖啡',
-    imgUrl: 'https://picsum.photos/151',
-  }]
-}]
+export const journeyList = [
+  {
+    name: "洧杰行程",
+    type: "夜間首選",
+    journey: [
+      {
+        name: "海水屋100元熱炒",
+        imgUrl: "https://picsum.photos/140",
+      },
+      {
+        name: "渣男串燒",
+        imgUrl: "https://picsum.photos/141",
+      },
+      {
+        name: "錢櫃KTV",
+        imgUrl: "https://picsum.photos/142",
+      },
+      {
+        name: "新永和豆漿",
+        imgUrl: "https://picsum.photos/143",
+      },
+    ],
+  },
+  {
+    name: "筱清行程",
+    type: "放鬆養生",
+    journey: [
+      {
+        name: "楓上SPA會館",
+        imgUrl: "https://picsum.photos/144",
+      },
+      {
+        name: "碳佐麻里燒肉",
+        imgUrl: "https://picsum.photos/145",
+      },
+      {
+        name: "白河溫泉",
+        imgUrl: "https://picsum.photos/146",
+      },
+      {
+        name: "山野Bar",
+        imgUrl: "https://picsum.photos/147",
+      },
+    ],
+  },
+  {
+    name: "Tina行程",
+    type: "城市郊遊",
+    journey: [
+      {
+        name: "早安公雞",
+        imgUrl: "https://picsum.photos/148",
+      },
+      {
+        name: "凱擘影院",
+        imgUrl: "https://picsum.photos/149",
+      },
+      {
+        name: "後站商圈_府中",
+        imgUrl: "https://picsum.photos/150",
+      },
+      {
+        name: "秘境咖啡",
+        imgUrl: "https://picsum.photos/151",
+      },
+    ],
+  },
+];
 //ex夜間首選只挑兩間or全挑，有缺再繼續補
-export const spots = [{
-  name: '海水屋100元熱炒',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '渣男串燒',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '錢櫃KTV',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '新永和豆漿',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '楓上SPA會館',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '碳佐麻里燒肉',
-  imgUrl: '',
-}, {
-  name: '白河溫泉',
-  imgUrl: '',
-}, {
-  name: '山野Bar',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '早安公雞',
-  imgUrl: '',
-}, {
-  name: '凱擘影院',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '三多商圈',
-  imgUrl: '',
-}, {
-  name: '秘境咖啡',
-  imgUrl: '',
-}, {
-  name: '望高咖啡',
-  type: ['夜間首選'],
-  imgUrl: '',
-}, {
-  name: '國家展院動畫故事館',
-  type: ['鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '高雄藝文中心',
-  type: ['鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '米詩堤-極北藍點',
-  imgUrl: '',
-}, {
-  name: '旗山老街',
-  type: ['鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '客家文化村',
-  type: ['鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '181眷村',
-  type: ['鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '九三彩繪牆',
-  type: ['鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '開漳聖王廟口夜市',
-  type: ['夜間首選', '鄉土文化'],
-  imgUrl: '',
-}, {
-  name: '劉家酸白菜鍋',
-  imgUrl: '',
-}]
+export const spots = [
+  {
+    name: "海水屋100元熱炒",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "渣男串燒",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "錢櫃KTV",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "新永和豆漿",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "楓上SPA會館",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "碳佐麻里燒肉",
+    imgUrl: "",
+  },
+  {
+    name: "白河溫泉",
+    imgUrl: "",
+  },
+  {
+    name: "山野Bar",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "早安公雞",
+    imgUrl: "",
+  },
+  {
+    name: "凱擘影院",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "三多商圈",
+    imgUrl: "",
+  },
+  {
+    name: "秘境咖啡",
+    imgUrl: "",
+  },
+  {
+    name: "望高咖啡",
+    type: ["夜間首選"],
+    imgUrl: "",
+  },
+  {
+    name: "國家展院動畫故事館",
+    type: ["鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "高雄藝文中心",
+    type: ["鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "米詩堤-極北藍點",
+    imgUrl: "",
+  },
+  {
+    name: "旗山老街",
+    type: ["鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "客家文化村",
+    type: ["鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "181眷村",
+    type: ["鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "九三彩繪牆",
+    type: ["鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "開漳聖王廟口夜市",
+    type: ["夜間首選", "鄉土文化"],
+    imgUrl: "",
+  },
+  {
+    name: "劉家酸白菜鍋",
+    imgUrl: "",
+  },
+];
